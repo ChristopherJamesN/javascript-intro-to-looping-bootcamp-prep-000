c415ccafd3441518802a1effc981db570310fddc
Chris Nady <nady.christopher@gmail.com>
2017-08-25 21:24:46 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,29 +1,32 @@
-
-function forLoop(array) {
-  for (let i = 0; i < 25; i++) {
-  if i === 1 {
-    array[i] = "I am one strange loop.";
-  } else {
-    array[i] = "I am ${i} strange loops.";
-    }
-  }
-  return array;
+function maybeTrue() {
+  return Math.random() >= 0.5 // Returns a random number between 0 (inclusive) and 1 (exclusive)
 }
 
-function whileLoop(n) {
-  while (n>0){
-    console.log(n);
+function forLoop(array) {
+  var length = array.length
+  for (var i = 0; i < 25; i++) {
+    if (i === 1) {
+      array[i+length] = "I am one strange loop.";
+    } else {
+      array[i+length] = "I am ${i} strange loops.";
+    }
   }
+    return array;
+  }
+
+
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(n)
+    n--
+  }
+  return "done";
 }
 
 function doWhileLoop(array) {
-  function maybeTrue() {
-    return Math.random() >= 0.5
-  }
-
   do {
-    array.unshift();
-  } while (array.length>0 && maybeTrue());
+    array.shift();
+  } while (array.length > 0 && maybeTrue());
 
   return array;
 }
